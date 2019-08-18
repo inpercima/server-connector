36415fc17269d7814d8cfa0582098b4df5ae3a0b
inpercima <inpercima@gmail.com>
2019-08-18 09:24:25 +0200

    Log with slf4j

--- src/main/java/net/inpercima/minecraftstarter/ServerApplication.java
+++ src/main/java/net/inpercima/minecraftstarter/ServerApplication.java
@@ -14,65 +14,70 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class ServerApplication implements CommandLineRunner {
 
-	@Autowired
-	private ServerProperties properties;
+    @Autowired
+    private ServerProperties properties;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
-	@Override
+    @Override
     public void run(String... strings) throws Exception {
         connect();
     }
 
-	private void connect() {
-		try {
-			final Properties config = new Properties();
-			config.put("StrictHostKeyChecking", "no");
-			final JSch jsch = new JSch();
-			final Session session = jsch.getSession(properties.getUser(), properties.getHost(), properties.getPort());
-			session.setPassword(properties.getPassword());
-			session.setConfig(config);
-			session.connect();
-			System.out.println("Connected");
+    private void connect() {
+        try {
+            final Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            final JSch jsch = new JSch();
+            final Session session = jsch.getSession(properties.getUser(), properties.getHost(), properties.getPort());
+            session.setPassword(properties.getPassword());
+            session.setConfig(config);
+            session.connect();
+            log.info("Connected");
 
-			final Channel channel = session.openChannel("exec");
+            final Channel channel = session.openChannel("exec");
 
-			final String commands = properties.getCommands().stream().map(command -> command.toString()).collect(Collectors.joining(" ; "));
-			((ChannelExec) channel).setCommand(commands);
+            final String commands = properties.getCommands().stream().map(command -> command.toString())
+                    .collect(Collectors.joining(" ; "));
+            ((ChannelExec) channel).setCommand(commands);
 
-			channel.setInputStream(null);
-			((ChannelExec) channel).setErrStream(System.err);
+            channel.setInputStream(null);
+            ((ChannelExec) channel).setErrStream(System.err);
 
-			final InputStream in = channel.getInputStream();
-			channel.connect();
-			final byte[] tmp = new byte[1024];
-			while (true) {
-				while (in.available() > 0) {
-					final int i = in.read(tmp, 0, 1024);
-					if (i < 0)
-						break;
-					System.out.print(new String(tmp, 0, i));
-				}
-				if (channel.isClosed()) {
-					System.out.println("exit status: " + channel.getExitStatus());
-					break;
-				}
-				try {
-					Thread.sleep(1000);
-				} catch (final Exception ee) {
-				}
-			}
-			channel.disconnect();
-			session.disconnect();
-			System.out.println("DONE");
-		} catch (final Exception e) {
-			e.printStackTrace();
-		}
-	}
+            final InputStream in = channel.getInputStream();
+            channel.connect();
+            final byte[] tmp = new byte[1024];
+            while (true) {
+                while (in.available() > 0) {
+                    final int i = in.read(tmp, 0, 1024);
+                    if (i < 0) {
+                        break;
+                    }
+                    log.info("Server output: {}", new String(tmp, 0, i));
+                }
+                if (channel.isClosed()) {
+                    log.info("exit status: {}", channel.getExitStatus());
+                    break;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (final Exception ee) {
+                }
+            }
+            channel.disconnect();
+            session.disconnect();
+            log.info("DONE");
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
